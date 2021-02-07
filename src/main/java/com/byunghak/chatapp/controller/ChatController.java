2f9197031087434da900f07fd3d8d8015f7b55fb
bengHak <qkwl4678@naver.com>
2021-02-08 00:36:41 +0900

    feat: one on one chatting

---
+++ src/main/java/com/byunghak/chatapp/controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.byunghak.chatapp.controller;
+
+import com.byunghak.chatapp.dto.ChatRoom;
+import com.byunghak.chatapp.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
+}
