b8849a76089d4b87ba1fc15f983ff0680eb7eed0
bengHak <qkwl4678@naver.com>
2021-02-08 15:18:44 +0900

    feat: chatroom enter, quit

--- src/main/java/com/byunghak/chatapp/model/ChatMessage.java
+++ src/main/java/com/byunghak/chatapp/model/ChatMessage.java
@@ -1,16 +1,28 @@
 package com.byunghak.chatapp.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ChatMessage {
+
+    @Builder
+    public ChatMessage(MessageType type, String roomId, String sender, String message, long userCount) {
+        this.type = type;
+        this.roomId = roomId;
+        this.sender = sender;
+        this.message = message;
+        this.userCount = userCount;
+    }
+
     public enum MessageType {
-        ENTER, TALK
+        ENTER, QUIT, TALK
     }
     private MessageType type; // 메시지 타입
     private String roomId; // 방번호
     private String sender; // 메시지 보낸사람
     private String message; // 메시지
+    private long userCount; // 채팅방 인원수, 채팅방 내에서 메시지가 전달될 때 인원수 갱신시 사용
 }
